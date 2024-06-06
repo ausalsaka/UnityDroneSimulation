a3d6734feed79a01873bd88b190a6e9458bdb6ea
ausalsaka <76986320+ausalsaka@users.noreply.github.com>
2024-06-05 19:19:29 -0700

    Initial Commit
    Contains:
    Unity simulation environment
    Physics-based drone autopilot script
    YoloV9 object recognition script
    
    Also contains free assets from Unity store

---
+++ UnityDroneSim-master/Assets/Scripts/ObjectRecognition.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRecognition : MonoBehaviour
+{
+    void getWorldCoords() {
+        projectCoords(getImgCoords());
+
+    }
+
+    void projectCoords(double[] imgCoords) { 
+        // convert img coordinates to world coordinates
+    }
+    double[] getImgCoords() {
+        return null;
+    }
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ UnityDroneSim-master/README.txt
@@ -0,0 +1,8 @@
+Object-recognition based autonomous drone flight
+
+uses YoloV9 for object recognition
+
+IMPORTANT:
+to use YoloV9, a model is required. You can train one yourself, or download
+a pre-trained model from this google drive link:
+https://drive.google.com/drive/folders/1WXdilHSoRdJzmdJWMJTZoXnwXUE5k4yc?usp=sharing
